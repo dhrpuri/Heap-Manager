6245fe1c4f9606d3e9752fbfcd3a3a57b371f754
Nick Balkissoon <nickbalkissoon@Nicks-MBP.wireless.duke.local>
2015-01-21 23:03:37 -0500

    day 2 late night work sesh

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 #You can use either a gcc or g++ compiler
 #CC = g++
 CC = gcc
-EXECUTABLES = test_basic test_coalesce test_stress1 test_stress2
+EXECUTABLES = test_basic test_coalesce test_stress1 test_stress2 test_stress3
 CFLAGS = -I. -Wall -lm -DNDEBUG -g
 #Disable the -DNDEBUG flag for the printing the freelist
 #CFLAGS = -Wall -I.
@@ -30,6 +30,8 @@ test_stress1: test_stress1.c dmm.o
 	$(CC) $(CFLAGS) -o test_stress1 test_stress1.c dmm.o
 test_stress2: test_stress2.c dmm.o
 	$(CC) $(CFLAGS) -o test_stress2 test_stress2.c dmm.o
+test_stress3: test_stress3.c dmm.o
+	$(CC) $(CFLAGS) -o test_stress3 test_stress3.c dmm.o
 dmm.o: dmm.c
 	$(CC) $(CFLAGS) -c dmm.c 
 clean:
--- dmm.c
+++ dmm.c
@@ -2,10 +2,6 @@
 #include <unistd.h> //needed for sbrk
 #include <assert.h> //For asserts
 #include "dmm.h"
-/* You can improve the below metadata structure using the concepts from Bryant
- * and OHallaron book (chapter 9).
- */
-
 
 typedef struct metadata {
 
@@ -16,20 +12,20 @@ typedef struct metadata {
 	* size contains the size of the data object or the amount of free
  	* bytes 
 	*/
-	size_t size;
-	struct metadata* next;
-	struct metadata* prev; //What's the use of prev pointer?
-	bool free;
-} metadata_t;
+ 	size_t size;
+ 	struct metadata* next;
+ 	struct metadata* prev;
+ 	bool free;
+ } metadata_t;
 
 /* freelist maintains all the blocks which are not in use; freelist is kept
  * always sorted to improve the efficiency of coalescing 
  */
 
-static metadata_t* freelist = NULL;
+ static metadata_t* freelist = NULL;
 
-void* dmalloc(size_t numbytes) {
-	
+ void* dmalloc(size_t numbytes) {
+
 	if(freelist == NULL) { 			//Initialize through sbrk call first time
 		if(!dmalloc_init())
 			return NULL;
@@ -42,82 +38,75 @@ void* dmalloc(size_t numbytes) {
 	 * malloc it, otherwise we call sbrk to move the heap pointer up.
 	 */
 
-metadata_t* current = freelist;
-while (current && !(current->free && current->size >= numbytes)) {
-    current = current->next;
-}
-if (current==NULL){
- return NULL;
-}
+	 metadata_t* current = freelist;
+	 while (current && !(current->free && current->size >= numbytes)) {
+	 	current = current->next;
+	 }
+	 if (current==NULL){
+	 	return NULL;
+	 }
 
-	current->free = false;
+	 current->free = false;
 
-metadata_t* newblock = current + numbytes + METADATA_T_ALIGNED;  
-newblock->free = true;
+// take care of case 3
+	 if (current->size >= (numbytes + METADATA_T_ALIGNED)) {
+	 	metadata_t* newblock = current + numbytes + METADATA_T_ALIGNED;  
+	 	newblock->free = true;
 
+	 	newblock->size = current->size - numbytes - METADATA_T_ALIGNED;
+	 	current->size = numbytes;
 
-
-newblock->size = current->size - numbytes - METADATA_T_ALIGNED;
-current->size = numbytes;
-
-
-newblock->next = current->next;
-current->next = newblock;
-
-
-if(newblock->next){
-	newblock->next->prev = newblock;
-}
-
-newblock->prev = current;
+	 	newblock->next = current->next;
+	 	current->next = newblock;
 
 
-return newblock + METADATA_T_ALIGNED;
+	 	if(newblock->next){
+	 		newblock->next->prev = newblock;
+	 	}
 
-}
+	 	newblock->prev = current;
 
-
-void dfree(void* ptr) {
-	
-	printf("%d\n", ptr);
-	metadata_t* block_to_free =  (metadata_t*) ptr;// - METADATA_T_ALIGNED;
-	printf("%d\n", block_to_free);
-	if (block_to_free){
-
-	block_to_free->free = true;
+	 }
 
 
-//coallesing
-
-
-	//metadata_t* previous_block = block_to_free->prev;
-	//metadata_t* next_block = block_to_free->next;
-
-	// 	if (previous_block){
-
-// 	if (previous_block->free){
-// 	printf("%s\n","we here" );
-// 	previous_block->size = previous_block->size+ block_to_free->size + METADATA_T_ALIGNED;
-// 	previous_block->next = next_block;
-
-// 	if (next_block && next_block->free){
-// 	previous_block->size = previous_block->size+ next_block->size + METADATA_T_ALIGNED;
-// 	previous_block->next = next_block->next;
-// 	}
-// }
-// }
-// else if (next_block){
-// 	if (next_block->free){
-// 	block_to_free->size = block_to_free->size+ next_block->size + METADATA_T_ALIGNED;
-// 	block_to_free->next = next_block->next;
-// }
-// }
+	 return ((void*)current) + METADATA_T_ALIGNED;
 
 	}
 
-}
 
-bool dmalloc_init() {
+	void dfree(void* ptr) {
+
+		metadata_t* block_to_free = ((metadata_t*) ptr) - 1;
+
+		if (!block_to_free){
+			return;
+		}
+		block_to_free->free = true;
+
+		//coallesing
+
+		metadata_t* previous_block = block_to_free->prev;
+		metadata_t* next_block = block_to_free->next;
+
+		if (previous_block && previous_block->free){
+			previous_block->size = previous_block->size+ block_to_free->size + METADATA_T_ALIGNED;
+			previous_block->next = next_block;
+
+			if (next_block && next_block->free){
+				previous_block->size = previous_block->size+ next_block->size + METADATA_T_ALIGNED;
+				previous_block->next = next_block->next;
+			}
+			
+		}
+		else if (next_block && next_block->free){
+			block_to_free->size = block_to_free->size+ next_block->size + METADATA_T_ALIGNED;
+			block_to_free->next = next_block->next;
+		}
+
+
+	}
+
+	bool dmalloc_init() {
 
 	/* Two choices: 
  	* 1. Append prologue and epilogue blocks to the start and the end of the freelist
@@ -127,7 +116,7 @@ bool dmalloc_init() {
  	* corner cases succinctly.
  	*/
 
-	size_t max_bytes = ALIGN(MAX_HEAP_SIZE);
+ 	size_t max_bytes = ALIGN(MAX_HEAP_SIZE);
 	freelist = (metadata_t*) sbrk(max_bytes); // returns heap_region, which is initialized to freelist
 	/* Q: Why casting is used? i.e., why (void*)-1? */
 	if (freelist == (void *)-1)
@@ -136,6 +125,7 @@ bool dmalloc_init() {
 	freelist->prev = NULL;
 	freelist->size = max_bytes-METADATA_T_ALIGNED;
 	freelist->free = true;
+
 	return true;
 }
 
@@ -143,7 +133,7 @@ bool dmalloc_init() {
 void print_freelist() {
 	metadata_t *freelist_head = freelist;
 	while(freelist_head != NULL) {
-		printf("\tFreelist Size:%zd, Head:%p, Prev:%p, Next:%p, Free:%d\t\n",freelist_head->size,freelist_head,freelist_head->prev,freelist_head->next,freelist_head->free);
+		DEBUG("\tFreelist Size:%zd, Head:%p, Prev:%p, Next:%p, Free:%d\t\n",freelist_head->size,freelist_head,freelist_head->prev,freelist_head->next,freelist_head->free);
 		freelist_head = freelist_head->next;
 	}
 	DEBUG("\n");
